66b462a1111d92cb1777fa3171496946c9fbcc1d
p4sticcio <58007020+p4sticcio@users.noreply.github.com>
2019-11-20 23:53:32 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -7,7 +7,7 @@ if($msg == "/start")
 {
 sm($chatID, "Benvenuto nel mio bot! da qui potrai leggere le ultime notizie.");
 }
-
+/*
 // ECHO
 if(!$update)
 {
@@ -30,3 +30,4 @@ header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => $text);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
+*/
